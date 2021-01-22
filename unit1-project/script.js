330bb1cb6be1131e71cf172f641740a816d1422e
LG29878 <grajalesliliana@johndeere.com>
2021-01-22 09:02:25 -0500

    reset and next button, still working on it

--- unit1-project/script.js
+++ unit1-project/script.js
@@ -1,12 +1,27 @@
 
+//Variables Initialization
 let score=0;
-let movingWords =document.querySelectorAll(".word");
-let scoreDisplay =document.querySelector(".score");
+var seconds = 0;
+let secondsLimit =30;
 let count =0;
+let Tcount =0;
 let aIndex = 12; //initial a.length
+let sayingsMax =12; //Number of sayings available in the game
+let remainingSeconds = 30;// Start of seconds countdown
+let a =[];//Initializing a
+
+let saying=document.querySelector("p");
+let movingWords =document.querySelectorAll(".word");
+let wordTags = document.getElementsByClassName("word");
+var el = document.getElementById('countdown');
+let scoreDisplay =document.querySelector(".score");
+let buttonNext = document.querySelector('.next');
+let buttonReset = document.querySelector('.reset');
+
 
 //shuffle index array a=index numbers for each array of (sayings, words, score)
 //let a = [0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+function indexShuffle(){
 let a = [0, 1, 2, 3, 4, 5,6,7,8,9,10,11]
 for (var i = 0; i < 100; i++)
 {
@@ -16,8 +31,11 @@ var tmp = a[index1]; //store in temp during shuffle
 a[index1] = a[index2]; //change 
 a[index2] = tmp;
 }
+return a;
+}
+a = indexShuffle()
 aIndex = a.pop();
-
+//End of index array shuffle
 
 function wordClickEvent(e) {
   count +=1;
@@ -28,17 +46,17 @@ function wordClickEvent(e) {
   
   let wordId = parseInt(e.target.id,10);
   let wordValue = wordScore[aIndex][wordId];
+
   if (wordValue == 10){  
        e.target.classList.add('green');
     } else {e.target.classList.add('red'); }
-       //below removes listener from that specific cell
-      // e.target.removeEventListener('click',runGame);
-      
-      score += wordValue;
-      scoreDisplay.innerText = "Accumulated Score: " + score
+  score += wordValue;
+  perc = ((score/(score*count)) * 100).toFixed(1)
+  scoreDisplay.innerHTML = "Accumulated points: " + score + "<br>" + "You did "+count+ " clicks. <br>" +
+  "Your click success rate was " + perc + " %"
 
-   if (count==a.lenth){
-    console.log("You did 10 Sayings, good JOB!! This game is over")
+  if (a.length<=1){
+    alert("You did " + count + " sayings, good JOB!! This game is over. " + "<br>" +" Your final accumulated points: " + score + " points per clicks " + perc + " %" );
     } 
 }
 movingWords[0].addEventListener('click',wordClickEvent);
@@ -57,7 +75,7 @@ function readSay(num){
   "Easier said than ___",
   "If you cannot beat them, ___ them",
   "if you pay peanuts, you get ____",
-  "it is not use ____ over spilt milk",
+  "it is no use ____ over spilled milk",
   "Never say ____",
   "No man is an ____",
   "look before you ___"
@@ -70,17 +88,13 @@ function readSay(num){
   ["done","carved","buried","trashed","cooked","placed"],
   ["scare","join","beat","silence","smile at","forget"],
   ["roasted","butter","squirrels","salt","monkeys","hungry"],
-  ["singing","yelling","laughin","crying","tearing","sobbing"],
+  ["singing","yelling","laughing","crying","tearing","sobbing"],
   ["always","forever","never","tomorrow","now","when"],
   ["ignorant","ego","elephant","astronaut","igloo","island"],
-  ["climb","leap","run","fly","lie","rest"]];
-
-
+  ["climb","leap","run","fly","cross","rest"]];
   let aIndex = num;
-  let saying=document.querySelector("p");
   saying.innerText=  sayingList[aIndex];
-
-  let wordTags = document.getElementsByClassName("word");
+  
   for (let i=0; i<6;i++){
     wordTags[i].innerText = wordList[aIndex][i];
   }
@@ -88,14 +102,18 @@ function readSay(num){
 
 function reset(){
   score=0;
+  seconds = 0;
   for(let i=0; i<movingWords.length; i++) {
   movingWords[i].classList.remove('green');
   movingWords[i].classList.remove('red');
   scoreDisplay.innerText = "Accumulated Score: " + score
   movingWords[i].addEventListener('click',wordClickEvent,{once:true});
   }
+  a = indexShuffle()
+  readSay(aIndex)
 }
 function next(){
+  seconds=0;
   for(let i=0; i<6; i++) {
     movingWords[i].classList.remove('green');
     movingWords[i].classList.remove('red');
@@ -104,12 +122,23 @@ function next(){
   readSay(aIndex)
   return aIndex
 }
+
+
 readSay(aIndex) 
 
-let buttonReset = document.querySelector('.reset');
 buttonReset.addEventListener('click',reset);
-let buttonNext = document.querySelector('.next');
+
 buttonNext.addEventListener('click',next);
 
-  
+ //Clock
+
+  function incrementSeconds() {
+     seconds += 1;
+     remainingSeconds = secondsLimit - seconds;
+     el.innerText = "This saying has " + remainingSeconds + " seconds reamining.";
+     if (remainingSeconds<=0){
+      next()
+     }
+ }
+ var cancel = setInterval(incrementSeconds, 1000);
 
